363c62b0fa720b60de363efd298fb8c3fa66ee20
Will Brodhead <brodheadw@gmail.com>
2025-07-05 15:46:10 -0400

    Remove build/ dir from git tracking

--- build/src/CMakeFiles/ggml_utils.dir/utils/logger.cpp.o.d
+++
@@ -1,2 +0,0 @@
-src/CMakeFiles/ggml_utils.dir/utils/logger.cpp.o: \
-  /Users/willb/Vaults/Personal/DevLab/ggml/ggml-viz/src/utils/logger.cpp
--- build/third_party/ggml/src/CMakeFiles/ggml-cpu.dir/ggml-cpu/hbm.cpp.o.d
+++
@@ -1,2 +0,0 @@
-third_party/ggml/src/CMakeFiles/ggml-cpu.dir/ggml-cpu/hbm.cpp.o: \
-  /Users/willb/Vaults/Personal/DevLab/ggml/ggml-viz/third_party/ggml/src/ggml-cpu/hbm.cpp
