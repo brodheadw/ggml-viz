f73ef96fd0ac94f6e60d48b7bd4745144844e844
Will Brodhead <brodheadw@gmail.com>
2025-07-16 12:12:32 -0400

    Added if(NOT TARGET ggml_ipc) guard around lib definition

---
+++ build-release/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /Users/willb/Vaults/Personal/DevLab/ggml/ggml-viz
+# Build directory: /Users/willb/Vaults/Personal/DevLab/ggml/ggml-viz/build-release
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("third_party")
+subdirs("third_party/ggml")
+subdirs("src")
+subdirs("tests")
