3d3fb538c9228d3e91749732a2fed4cb4c289079
Will Brodhead <brodheadw@gmail.com>
2025-07-25 10:46:37 -0400

    Added unitialized members in ggml_hook.cpp private constructor

--- src/instrumentation/ggml_hook.cpp
+++ src/instrumentation/ggml_hook.cpp
@@ -61,8 +61,8 @@ namespace {
     }
 }
 
-// Private constructor with environment variable initialization
-GGMLHook::GGMLHook() {
+// Private constructor with environment variable initialization  
+GGMLHook::GGMLHook() : event_buffer_{}, write_pos_{}, read_pos_{} {
     // Initialize config with defaults, then override with environment variables
     config_.enable_op_timing = true;
     config_.enable_memory_tracking = false;
